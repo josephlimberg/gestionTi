ce6d12976258ba8b8d760b9b003deefd0313fbf6
josephlimberg <joseph.limbergch@gmail.com>
2024-12-08 16:06:02 -0500

    Springboot

--- src/main/java/gm/contactos/security/UserConfig.java
+++ src/main/java/gm/contactos/security/UserConfig.java
@@ -13,19 +13,37 @@ public class UserConfig {
 
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-        // Usuario "user"
-        UserDetails user = User.withUsername("user")
-                .password(passwordEncoder.encode("user"))
-                .roles("USER") // Rol USER
-                .build();
+
 
         // Nuevo usuario "admin"
         UserDetails admin = User.withUsername("admin")
                 .password(passwordEncoder.encode("admin"))
                 .roles("ADMIN") // Rol ADMIN
                 .build();
+        // Nuevo usuario "admin"
+        UserDetails admin2 = User.withUsername("juanadmin@correo.com")
+                .password(passwordEncoder.encode("juanadmin"))
+                .roles("ADMIN") // Rol ADMIN
+                .build();
+
+        // Usuario "user"
+        UserDetails user = User.withUsername("miauser1@correo.com")
+                .password(passwordEncoder.encode("miauser1"))
+                .roles("USER") // Rol USER
+                .build();
+
+        // Nuevo usuario "user1"
+        UserDetails user1 = User.withUsername("felipeuser@correo.com")
+                .password(passwordEncoder.encode("felipeuser"))
+                .roles("USER") // Rol USER
+                .build();
+        // Nuevo usuario "user2"
+        UserDetails user2 = User.withUsername("mariauser1@correo.com")
+                .password(passwordEncoder.encode("mariauser1"))
+                .roles("USER") // Rol ADMIN
+                .build();
 
 
-        return new InMemoryUserDetailsManager(user, admin);
+        return new InMemoryUserDetailsManager(user, admin,admin2, user1, user2);
     }
 }
